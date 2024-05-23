c740be40db03178df8cffb9bf64722c9c5ae74af
alencarp <patriciaalencar775@gmail.com>
2024-05-23 18:39:04 -0300

    updated NextSaturdayDate

--- src/NextSaturdayDate.java
+++ src/NextSaturdayDate.java
@@ -1,12 +1,15 @@
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
 
 //Exercicio: Escreva um programa para mostrar a data do próximo sábado
 public class NextSaturdayDate {
     public static void main(String[] args) {
-        DayOfWeek currentDate = LocalDate.from(LocalDate.now()).getDayOfWeek();
-        System.out.println(currentDate);
+        LocalDate localDate = NextSaturdayDate.calcNextFriday(LocalDate.now());
+        System.out.println(localDate);
+    }
 
-
+    private static LocalDate calcNextFriday(LocalDate d) {
+        return d.with(TemporalAdjusters.next(DayOfWeek.SATURDAY));
     }
 }
